ab3f6c9b49d629ce0d8373822bea69010635169f
Ash Lux <ashlux@gmail.com>
2009-04-11 20:20:13 -0500

    Break out twitarr app to another repo.

--- twitarr/twitarr-twitter/src/main/java/com/ashlux/potbs/twitarr/writer/PotbsWriter.java
+++
@@ -1,9 +0,0 @@
-package com.ashlux.potbs.twitarr.writer;
-
-import com.ashlux.potbs4j.vo.ServerDocument;
-
-public interface PotbsWriter
-{
-    public void publishServerStatusChange( ServerDocument.Server server )
-        throws TwitarrWriterException;
-}
